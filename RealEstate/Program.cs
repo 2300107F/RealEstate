556d954a3fa9148d27f3acb48474c20f3714ad09
RAYNER TAN GUAN SHAN 2300107F <2300107F@student.tp.edu.sg>
2024-12-08 23:28:31 +0800

    Added custom Identity logic

--- RealEstate/Program.cs
+++ RealEstate/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<RealEstateUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<RealEstateContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
